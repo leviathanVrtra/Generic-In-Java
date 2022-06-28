43a5d0b3c21a3eba82da59fbd2ee1643af34d6cd
Sachin Verma <59794122+leviathanVrtra@users.noreply.github.com>
2022-06-28 22:40:49 +0530

    Bounded Generic

--- Main1/src/Main.java
+++ Main1/src/Main.java
@@ -5,5 +5,22 @@ public class Main {
 
         Printer<Dog> dogPrinter = new Printer<>(new Dog());
         dogPrinter.print();
+		
+		shout("Sachin", 111);
+
+        List l2 = new ArrayList();  // UnType Safe : Different type elements can be added
+        l2.add(new Cat());
+        l2.add(new Dog());
+        printList(l2);
+    }
+	
+	private static <T, V> void shout(T key, V val) {
+        System.out.println(key + " -> " + val);
+    }
+
+    private static <T extends Animal> void printList(List<T>lst) {  // List<? extends Animal> lst
+        for (T t : lst) {
+            System.out.println(t.getName());
+        }
     }
 }
